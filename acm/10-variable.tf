2a8cb8e1b46278eb7cd269049cf3b4ec82875715
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2020-01-27 22:31:45 +0900

    설명

--- acm/10-variable.tf
+++ acm/10-variable.tf
@@ -1,18 +1,18 @@
 
 variable "region" {
-  description = "The region to deploy the cluster in, e.g: us-east-1"
+  description = "생성될 리전."
   type        = string
   default     = "us-east-1"
 }
 
 variable "domain_root" {
-  description = ""
+  description = "Route53 에 등록된 도메인 명"
   type        = string
   default     = "mzdev.be"
 }
 
 variable "domain_name" {
-  description = ""
+  description = "ACM 인증서를 생성할 도메인 명"
   type        = string
   default     = "demo-api-workshop.mzdev.be"
 }
--- lambda/00-variable.tf
+++ lambda/00-variable.tf
@@ -1,66 +1,66 @@
 
 variable "region" {
-  description = "생성될 리전을 입력 합니다."
+  description = "생성될 리전."
   type        = string
   default     = "ap-northeast-2"
 }
 
 variable "name" {
-  description = "서비스 이름을 입력 합니다."
+  description = "서비스 이름."
   type        = string
   default     = "demo-api"
 }
 
 variable "stage" {
-  description = "서비스 영역을 입력 합니다."
+  description = "서비스 영역."
   type        = string
   default     = "dev"
 }
 
 variable "runtime" {
-  description = "람다 펑션이 실행될 런타임을 입력 합니다."
+  description = "람다 펑션이 실행될 런타임."
   type        = string
   default     = "nodejs10.x"
 }
 
 variable "handler" {
-  description = "람다 펑션이 실행될 핸들러 이름을 입력 합니다."
+  description = "람다 펑션이 실행될 핸들러 이름."
   type        = string
   default     = "index.handler"
 }
 
 variable "memory_size" {
-  description = "람다 펑션이 실행될 메모리 사이즈를 입력 합니다."
+  description = "람다 펑션이 실행될 메모리 사이즈."
   type        = string
   default     = "1024"
 }
 
 variable "timeout" {
-  description = "람다 펑션의 타임아웃 값을 입력 합니다."
+  description = "람다 펑션의 타임아웃 값."
   type        = string
   default     = "5"
 }
 
 variable "s3_bucket" {
-  description = "The S3 bucket location containing the function's deployment package."
+  description = "배포 패키지가 저장될 버켓 이름."
   type        = string
   default     = "terraform-workshop-seoul"
 }
 
 variable "s3_source" {
-  description = "The S3 source location containing the function's deployment package."
+  description = "복사될 배포 패키지가 있는 경로."
   type        = string
   default     = "package/lambda.zip"
 }
 
 variable "s3_key" {
-  description = "The S3 key of an object containing the function's deployment package."
+  description = "배포 패키지가 배포될 경로."
   type        = string
   default     = "package/lambda.zip"
 }
 
 variable "env_vars" {
-  description = "A map that defines environment variables for the Lambda function."
+  description = "람다 펑션에서 사용될 환경 변수 맵."
   type        = map(string)
   default = {
     "PROFILE" = "dev",
@@ -82,13 +82,13 @@ variable "http_methods" {
 }
 
 variable "domain_root" {
-  description = ""
+  description = "Route53 에 등록된 도메인 명"
   type        = string
   default     = "mzdev.be"
 }
 
 variable "domain_name" {
-  description = ""
+  description = "람다 펑션이 서비스 될 도메인 명"
   type        = string
-  default     = "demo-api-workshop.mzdev.be"
+  default     = "demo-api.workshop.mzdev.be"
 }
