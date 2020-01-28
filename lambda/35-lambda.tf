51655f0425a39b24c83c905574b4e2646caa98c5
nalbam <me@nalbam.com>
2020-01-28 14:59:48 +0900

    aws_lambda_permission

--- lambda/35-lambda.tf
+++ lambda/35-lambda.tf
@@ -29,3 +29,15 @@ resource "aws_lambda_function" "default" {
     variables = var.env_vars
   }
 }
+
+resource "aws_lambda_permission" "default" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.default.arn
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecutionFromAPIGateway"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  //source_arn = "${aws_api_gateway_deployment.default.execution_arn}/${aws_api_gateway_method.default_get_req.http_method}${aws_api_gateway_resource.default.path}"
+  //source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.default.id}/*/${aws_api_gateway_method.default.http_method}${aws_api_gateway_resource.default.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.default.id}/*/*"
+}
