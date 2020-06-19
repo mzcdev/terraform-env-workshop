f5c62a215df64c397db4b9b19a7d1d23ff3193b7
nalbam <me@nalbam.com>
2020-06-19 19:43:20 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,20 @@ export BUCKET="terraform-workshop-${ACCOUNT_ID}"
 echo "REGION=${REGION}"
 echo "BUCKET=${BUCKET}"
 
+# replace workshop name
 if [ "${OS_NAME}" == "darwin" ]; then
     find . -name '*.tf' -exec sed -i '' -e "s/terraform-workshop-[[:alnum:]]*/${BUCKET}/g" {} \;
 else
     find . -name '*.tf' -exec sed -i -e "s/terraform-workshop-[[:alnum:]]*/${BUCKET}/g" {} \;
 fi
+
+# create s3 bucket
+aws s3 mb s3://${BUCKET} --region ${REGION}
+
+# create dynamodb table
+aws dynamodb create-table \
+    --table-name ${BUCKET} \
+    --attribute-definitions AttributeName=LockID,AttributeType=S \
+    --key-schema AttributeName=LockID,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --region ${REGION}
