f60c169a5c6240ea2b268119419bd3540119f14a
nalbam <me@nalbam.com>
2020-06-19 19:32:43 +0900

    add replace

---
+++ replace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+# variable
+export ACCOUNT_ID=$(aws sts get-caller-identity | jq .Account -r)
+
+echo "ACCOUNT_ID=${ACCOUNT_ID}"
+
+export REGION="ap-northeast-2"
+export BUCKET="terraform-workshop-${ACCOUNT_ID}"
+
+echo "REGION=${REGION}"
+echo "BUCKET=${BUCKET}"
+
+if [ "${OS_NAME}" == "darwin" ]; then
+    find . -name '*.tf' -exec sed -i '' -e "s/terraform-workshop-mzcdev/${BUCKET}/g" {} \;
+else
+    find . -name '*.tf' -exec sed -i -e "s/terraform-workshop-mzcdev/${BUCKET}/g" {} \;
+fi
